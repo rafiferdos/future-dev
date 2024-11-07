1ec8796e06886434bc60b01f6c1337b8f630b6de
mstsurnalyakter <surnalyms@gmail.com>
2024-11-08 01:42:14 +0600

    theme and font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: '"Inter", serif',
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-  plugins: [
-    daisyui,
-  ],
-}
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        foodCycle: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#224084",
+          secondary: "#FFF8E1",
+          ".btn-primary": {
+            color: "#fff",
+          },
+          ".btn-outline.btn-primary:hover": {
+            color: "#fff",
+          },
+        },
+      },
+    ],
+  },
+};
