9391fedee342e952f96264c746ff141597ad8dc7
mstsurnalyakter <surnalyms@gmail.com>
2024-11-14 05:24:43 +0600

    add: program section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
+import ProgramSection from '../ProgramSection/ProgramSection'
 
 const Home = () => {
   return (
-    <div className='space-y-20'>
+    <div className=''>
       <Banner/>
+      <ProgramSection/>
 
     </div>
   )
